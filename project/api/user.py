f35d2b0cb6a1185156764784a1ffcb77b78210b9
Sasha Repin <sasharepin2016@gmail.com>
2022-04-17 23:14:56 +0300

    changes env, added new endpoints, upload files

--- project/api/user.py
+++ project/api/user.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import make_response, request, jsonify, render_template
+from flask import make_response, request, jsonify, render_template, current_app
 from flask_login import current_user
 from werkzeug.utils import secure_filename
-
 from . import api
+
 
 # @api.route('/userava')
 # def userava():
@@ -17,11 +17,9 @@ from . import api
 #     return h
 
 
-
-
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in current_app.config['ALLOWED_EXTENSIONS']
 
 
 @api.route('/upload', methods=["POST"])
@@ -30,7 +28,7 @@ def upload():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
             return jsonify({'filename': filename})
 
     return jsonify({'msg': 'error'})
